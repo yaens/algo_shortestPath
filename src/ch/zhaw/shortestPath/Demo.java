f02da38a3547642485c29cbaddd56b3f69f1e78f
yaens <koppyan1@students.zhaw.ch>
2012-12-29 00:31:53 +0100

    added eclipse project, demo app, and dependencys + examples

---
+++ src/ch/zhaw/shortestPath/Demo.java
@@ -0,0 +1,32 @@
+package ch.zhaw.shortestPath;
+
+import gov.nasa.worldwind.BasicModel;
+import gov.nasa.worldwind.awt.WorldWindowGLCanvas;
+
+import javax.swing.*;
+
+/** This example demonstrates the simplest possible way to create a WorldWind application. */
+public class Demo extends JFrame
+{
+    public Demo()
+    {
+        WorldWindowGLCanvas wwd = new WorldWindowGLCanvas();
+        wwd.setPreferredSize(new java.awt.Dimension(1000, 800));
+        this.getContentPane().add(wwd, java.awt.BorderLayout.CENTER);
+        wwd.setModel(new BasicModel());
+    }
+
+    public static void main(String[] args)
+    {
+        java.awt.EventQueue.invokeLater(new Runnable()
+        {
+            public void run()
+            {
+                JFrame frame = new Demo();
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                frame.pack();
+                frame.setVisible(true);
+            }
+        });
+    }
+}
